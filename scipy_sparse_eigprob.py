0bb91af271016f7fca802dc685c4738d3e405288
Samarth G Kashyap <g.samarth@tifr.res.in>
2021-05-14 19:56:11 +0530

    added tf lines with scipy

--- scipy_sparse_eigprob.py
+++ scipy_sparse_eigprob.py
@@ -2,6 +2,7 @@ import numpy as np
 import scipy.linalg
 import  scipy.sparse
 import scipy.sparse.linalg
+import tensorflow as tf
 import time
 
 # creating the sparse matrix of a desired size
@@ -27,11 +28,16 @@ def solve_eigprob(n):
     for j in range(N_iter): evals, evecs = scipy.sparse.linalg.eigsh(mat)
     t2b = time.time()
 
-    return ((t1b-t1a)/N_iter, (t2b-t2a)/N_iter)
+    t3a = time.time()
+    mat = tf.convert_to_tensor(mat)
+    for j in range(N_iter): evals, evecs = tf.linalg.eigh(mat)
+    t3b = time.time()
+
+    return ((t1b-t1a)/N_iter, (t2b-t2a)/N_iter, (t3b-t3a)/N_iter)
 
 # timing computation of different size matrices                                                   
 N = np.logspace(1,3.5,6,dtype='int')
-print('--Martix size------non-sparse solver-------sparse solver---------')
+print('--Martix size------non-sparse solver-------sparse solver---------tensorflow-----')
 for i, n in enumerate(N):
-    time_eig, time_sparse_eigsh = solve_eigprob(n)
-    print(f'{n}x{n} matrix: {time_eig:.3f} seconds and {time_sparse_eigsh:.3f} seconds')
+    time_eig, time_sparse_eigsh, time_tf = solve_eigprob(n)
+    print(f'{n}x{n} matrix: {time_eig:.3f} seconds and {time_sparse_eigsh:.3f} seconds and {time_tf:.3f} seconds')
--- tf_eigprob.py
+++ tf_eigprob.py
@@ -14,8 +14,10 @@ def solve_eigprob(n, device="CPU"):
     N_iter = 10
     if device == "CPU":
         device_name = "CPU:0"
+        mat = build_matrix(n)
     elif device == "GPU":
         device_name = "GPU:0"
+        mat = tf.convert_to_tensor(build_matrix(n))
     matlist = []
     for j in range(N_iter):
         matlist.append(tf.convert_to_tensor(build_matrix(n)))
@@ -23,7 +25,11 @@ def solve_eigprob(n, device="CPU"):
     N_iter = 5
     with tf.device(device_name):
         for j in range(N_iter):
-            evals, evecs = tf.linalg.eigh(matlist[j])
+            # evals, evecs = tf.linalg.eigh(matlist[j])
+            if device == "CPU":
+                evals, evecs = np.linalg.eigh(mat)
+            else:
+                evals, evecs = tf.linalg.eigh(mat)
     t2 = time.time()
     return ((t2-t1)/N_iter)
 
@@ -32,7 +38,10 @@ N = np.logspace(1,3.5,6,dtype='int')
 time_arr = np.zeros_like(N,dtype='float')
 for i, n in enumerate(N):
     time_arr[i] = solve_eigprob(n, device="CPU")
+    tc = time_arr[i] * 1.0
     print(f'[CPU] {n:>4}x{n:<4} matrix: {time_arr[i]:7.4f} seconds.')
     time_arr[i] = solve_eigprob(n, device="GPU")
+    tg = time_arr[i] * 1.0
     print(f'[GPU] {n:>4}x{n:<4} matrix: {time_arr[i]:7.4f} seconds.')
+    print(f'GPU is {tc/tg:5.2f}x faster than CPU')
     print(f"{' ':*<30}")
